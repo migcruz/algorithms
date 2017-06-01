1c8b3230156756a41bc2be62556e5d46264ca9d1
migcruz <miguel.ramon.cruz@gmail.com>
2017-06-01 16:46:16 -0700

    gameday.cpp

---
+++ problems/gameday.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+
+using namespace std;
+
+struct team{
+	string name;
+	vector<team> teamsplayed;
+	int teamid;
+
+
+}
+
+
+int main(void){
+
+
+
+
+
+
+	return 0;
+}
